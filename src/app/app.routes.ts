30e3f38543658bc3501e932d25194696351346ad
Fellype Souza <fellypedev23@gmail.com>
2025-07-21 17:17:56 -0300

    recomeçando o projeto do 0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,6 @@
 import { Routes } from '@angular/router';
-import { ActivityDetailsPage } from '../pages/activity-details-page/activity-details-page';
-import { WorkoutPage } from '../pages/workout-page/workout-page';
-import { NewWorkoutPage } from '../pages/new-workout-page/new-workout-page';
-import { SpeedometerPage } from '../pages/speedometer-page/speedometer-page';
 
 export const routes: Routes = [
     //Página inicial
-    {
-        path: "",
-        component: WorkoutPage,
-    },
-    {
-        path: "newWorkout",
-        component: NewWorkoutPage,
-    },
+    
 ];
